d39dfa3a0e2f9998b5f929c25be1e2990750d436
MachineUserPTV <github@ptvgroup.com>
2023-01-31 14:02:23 +0000

    Automatic synchronisation from developer.myptv.com

--- typescript/runtime.ts
+++ typescript/runtime.ts
@@ -13,7 +13,7 @@
  */
 
 
-export const BASE_PATH = "https://api.staging.myptv.com/sequenceoptimization/v1".replace(/\/+$/, "");
+export const BASE_PATH = "https://api.myptv.com/sequenceoptimization/v1".replace(/\/+$/, "");
 
 const isBlob = (value: any) => typeof Blob !== 'undefined' && value instanceof Blob;
 
